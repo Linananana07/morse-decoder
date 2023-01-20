2fb905bec204ec51d5b62549bb745c64a2ce7ad6
Linananana07 <113578711+Linananana07@users.noreply.github.com>
2023-01-20 09:52:17 +0600

    fix: morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+    let arr = [];
+    let j = 0;
+    let res = '';
+    for (let i = 0; i <= expr.length; i++) {
+        if (i % 10 == 0 && i != 0) {
+            arr[j] = str;
+            j += 1;
+            str = '';
+        }
+        if (expr[i] == '0' && expr[i+1] == '0'){
+            i += 1;
+        } else if (expr[i] == '1' && expr[i + 1] == '0') {
+            str += '.';
+            i += 1;
+        } else if (expr[i] == '1' && expr[i + 1] == '1') {
+            str += '-';
+            i += 1;
+        } else if (expr [i] == '*') {
+            str += ' ';
+            i += 9;
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let key in MORSE_TABLE) {
+            if (arr[i] == key) {
+                res += MORSE_TABLE[key];
+            }
+        }
+        if (arr[i] == ' ') {
+            res += ' ';
+        }
+    }
+    const result = res;
+    console.log (result);
+    return result
 }
 
 module.exports = {
